120628b6d6182cb570cbf4f1dc123dd4ddaf07d7
angela-plana <angela.plana.7e7@itb.cat>
2023-11-09 09:55:17 +0100

    a

--- e5isAcorrectDate.py
+++ e5isAcorrectDate.py
@@ -4,13 +4,13 @@ dia = int(data.split("/") [0])
 mes = int(data.split("/") [1])
 anys = int(data.split("/") [2])
 
-
+#año bisiesto
 if anys %100 == 0:
     (anys/100) / 4
 if (anys) %4 == 0  and mes == 2 and dia <=29:
     print("Data valida")
 
-if dia in range(1,31 + 1) and mes
+#if dia in range(1,31 + 1) and mes
 
 
 
