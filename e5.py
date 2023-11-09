eaa4b811e38ce46d36873e31a287fa7f14a4f067
angela-plana <angela.plana.7e7@itb.cat>
2023-11-09 09:51:31 +0100

    a

---
+++ e5.py
@@ -0,0 +1,16 @@
+data = input("Escriu una data en format DD/MM/AAAA: ")
+
+dia = int(data.split("/") [0])
+mes = int(data.split("/") [1])
+anys = int(data.split("/") [2])
+
+
+if anys %100 == 0:
+    (anys/100) / 4
+if (anys) %4 == 0  and mes == 2 and dia <=29:
+    print("Data valida")
+
+if dia in range(1,31 + 1) and mes
+
+
+
